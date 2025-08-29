11f6089be3975553ac921346963a930388808e8b
Yara20031964 <yara200359@gmail.com>
2025-08-29 23:53:08 +0300

    done task 3

---
+++ Task3Search/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task3Search
+{
+    internal class Program
+    {
+        static string CheckVowel (string s)
+        {
+            s.ToLower ();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'i' || s[i] == 'o' || s[i] == 'a' || s[i] == 'e' || s[i] == 'u')
+                {
+                    throw new Exception("String that you entered contain vowel");
+                }
+            }
+            return "safe";
+        }
+        static void Main(string[] args)
+        {
+            //Task 1
+            //try
+            //{
+            //    List<int> list = new List<int>();
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine($"Please Enter number {i +1}");
+            //        int num = Convert.ToInt32( Console.ReadLine() );
+            //        if (list.Contains(num)){
+            //            throw new Exception("No duplicate number");
+            //        }
+            //        list.Add(num);
+
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Erorr: {ex.Message}");
+            //}
+
+            //=========================================================================================
+            //Task 2
+            try
+            {
+                Console.WriteLine("Please enter string that you need to check it ");
+                string s = Console.ReadLine();
+                Console.WriteLine(CheckVowel(s));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erorr: {e.Message}");
+            }
+
+        }
+    }
+}
